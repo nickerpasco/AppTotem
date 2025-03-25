f87f052c34ff270ffa79cbb17a202a056617e16c
nickerpasco <37748322+nickerpasco@users.noreply.github.com>
2025-03-24 23:09:57 -0500

    Subiendo el proyecto inicial

---
+++ app/src/main/java/com/example/apppagos/OrdenesPagoActivity.java
@@ -0,0 +1,14 @@
+package com.example.apppagos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OrdenesPagoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ordenes_pago);
+    }
+}
